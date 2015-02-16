a9de7a46692167be92558c1551f2cb360d8e83cb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2015-02-17 00:29:20 +0900

    リファクタリング

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 ENVBLD='cygwin'
 CURDIR=`pwd`
 
-for dir in . assembly assembly/bootsector assembly/ipl
+for dir in . assembly assembly/bootsector assembly/ipl kernel
 do
     cd  ${dir}  &&  rm -f  Makefile  \
         &&  ln -s  Makefile.${ENVBLD}  Makefile
