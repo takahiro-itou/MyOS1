aa98d06e634dccb6d0ce2ec28a1cc0707ef0888f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2015-02-21 17:51:16 +0900

    カーネル開始のテスト
    (1) 画面に文字列を表示

--- kernel/KernelMain.c
+++ kernel/KernelMain.c
@@ -1,4 +1,4 @@
-//  -*-  coding: utf-8; mode: asm  -*-  //
+//  -*-  coding: utf-8; mode: c++  -*-  //
 /*************************************************************************
 **                                                                      **
 **          KernelMain.c                                                **
@@ -10,5 +10,22 @@
 
 void  startKernel()
 {
+    int     i, j;
+    short * ptrVRAM = (short *)(0x000B8000);
+    const  char  *  buf = "Hello, World";
+
+    //  画面を消去。    //
+    for ( i = 0; i < 80 * 25; ++ i ) {
+        ptrVRAM[i]  = 0x0020;
+    }
+
+    //  文字列を表示。  //
+    for ( j = 0; j < 16; ++ j ) {
+        short * ptr = ptrVRAM + (j * 80);
+        for ( i = 0; buf[i] != '\0'; ++ i ) {
+            ptr[i]  = (j << 8) | (buf[i]);
+        }
+    }
+
     return;
 }
