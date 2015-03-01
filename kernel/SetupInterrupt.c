2c79b0debd96958c1e868b5df45ab20679660be9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2015-03-01 21:26:36 +0900

    割り込み設定
    (1) 設定用の領域を準備
    (2) 設定用の関数を準備

--- kernel/SetupInterrupt.c
+++ kernel/SetupInterrupt.c
@@ -13,3 +13,46 @@
 **
 **      @file   kernel/SetupInterrupt.c
 **/
+
+#include    "Descriptors.h"
+#include    "MemoryMap.h"
+
+typedef  struct
+{
+    unsigned short  ofsLow;
+    unsigned short  segment;
+    unsigned char   unused;
+    unsigned char   gdFlags;
+    unsigned short  ofsHigh;
+} GateDesc;
+
+//----------------------------------------------------------------
+/**
+**
+**/
+
+void
+setGateDesc(
+        const  int  idx,
+        const  int  offset,
+        const  int  segment,
+        const  int  flags)
+{
+    GateDesc *  gd  = (GateDesc *)(INTGATE_DESCRIPTOR_ADDR) + idx;
+    gd->ofsLow  = (offset & 0xFFFF);
+    gd->segment = segment;
+    gd->unused  = 0;
+    gd->gdFlags = (flags & 0xFF);
+    gd->ofsHigh = ((offset >> 16) & 0xFFFF);
+}
+
+//----------------------------------------------------------------
+
+void  setupIDT(void)
+{
+    int i;
+    for ( i = 0; i < 256; ++ i ) {
+        setGateDesc(i, 0, 0, 0);
+    }
+    __asm__ __volatile__ ( "lidt    (_IDT_POINT)");
+}
