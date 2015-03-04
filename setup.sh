ab01f660d16b0b68fb0999e664e6f7b2e83baa20
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2015-03-04 21:42:06 +0900

    リファクタリング

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ ENVBLD='cygwin'
 CURDIR=`pwd`
 
 for dir in . BootLoader BootLoader/bootsector BootLoader/initloader \
-             font src src/initkernel
+             data src src/initkernel
 do
     cd  ${dir}  &&  rm -f  Makefile  \
         &&  ln -s  Makefile.${ENVBLD}  Makefile
